e5d916dc69c9237a123cfe0782c88b355a516a65
Howard Huang <howardhuang2004@gmail.com>
2018-10-25 15:32:48 -0700

    trim

--- ftc_app-master M-Set Bettafish 2018 - 2019/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Intake.java
+++ ftc_app-master M-Set Bettafish 2018 - 2019/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Intake.java
@@ -2,11 +2,7 @@ package org.firstinspires.ftc.teamcode.TeleOp;
 
 public class Intake {
     org.firstinspires.ftc.teamcode.Robot r = new org.firstinspires.ftc.teamcode.Robot();
-<<<<<<< HEAD
-=======
 
-    Robot r = new Robot();
->>>>>>> ff3e8a00d04608224411129cfdd7c1d11e2e0214
     Gamepad gp1, gp2;
 
     public Intake(HardwareMap hm, Telemetry tm, Gamepad gp1, Gamepad gp2) {
@@ -18,10 +14,6 @@ public class Intake {
         this.gp1 = gp1;
         this.gp2 = gp2;
     }
-<<<<<<< HEAD
-
-=======
->>>>>>> ff3e8a00d04608224411129cfdd7c1d11e2e0214
 
     public void spin() {
 

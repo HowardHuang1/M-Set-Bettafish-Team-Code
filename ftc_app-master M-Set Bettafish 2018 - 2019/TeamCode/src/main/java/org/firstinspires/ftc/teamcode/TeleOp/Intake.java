7c7ec3c923166b50527d7cc3ca6b748696583cc5
Howard Huang <howardhuang2004@gmail.com>
2018-10-25 15:18:17 -0700

    Coded for Surgical Tubing Intake spin

--- ftc_app-master M-Set Bettafish 2018 - 2019/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Intake.java
+++ ftc_app-master M-Set Bettafish 2018 - 2019/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Intake.java
@@ -2,4 +2,17 @@ package org.firstinspires.ftc.teamcode.TeleOp;
 
 public class Intake {
     Robot r = new Robot();
+
+    public void spin() {
+
+        if (gamepad1.right_trigger)
+            //suck in
+            r.intake.setPower(1);
+        else if (gamepad1.left_trigger)
+            //spit out
+            r.intake.setPower(-1);
+        else
+            // halt all spinning
+            r.intake.setPower(0);
+    }
 }

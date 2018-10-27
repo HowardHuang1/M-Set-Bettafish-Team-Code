572208b9b7062ceb2d249b729c641ff2d4d891d0
TejBade <badetej5@gmail.com>
2018-10-26 19:43:26 -0700

    Revert "blue left"
    This reverts commit d1b938c53dcdb695a2f8eef6490a0fb8267f115e.

--- ftc_app-master M-Set Bettafish 2018 - 2019/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoOpBlueLeft.java
+++ ftc_app-master M-Set Bettafish 2018 - 2019/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoOpBlueLeft.java
@@ -1,11 +1,7 @@
-package org.firstinspires.ftc.teamcode.Autonomous;
+/*package org.firstinspires.ftc.teamcode.Autonomous;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-
-import org.firstinspires.ftc.teamcode.Robot;
-
-@Autonomous(name="AutoOpBlueLeft")
-public class AutoOpBlueLeft extends AutoOpBase {
+@AutoOpBlueLeft(name="AutoOpBlueLeft")
+public class AutoOpBlueLeft extends AutoOpBase{
     Robot r = new Robot();
 
     public void runOpMode() throws InterruptedException {
@@ -16,85 +12,18 @@ public class AutoOpBlueLeft extends AutoOpBase {
         while (opModeIsActive()) {
             r.winch.setPower(-speed);
             sleep(3400);
-            mecanumStrafeLeft(time);
+            MecanumStrafeLeft(ppower, time);
             speed = 0;
             time = 0;
             idle();
         }
         rest();
-        // turn right
-        turnLeft(3000);
-        // drive forward to align with wall
-        forward(5000);
-        // strafe to depot
-        mecanumStrafeLeft(10000);
     }
-
-    public void stopMotors() {
-        r.rightBack.setPower(0);
-        r.rightFront.setPower(0);
-        r.leftBack.setPower(0);
-        r.leftFront.setPower(0);
-    }
-
-    public void forward(int time) {
-        r.rightBack.setPower(-1);
-        r.rightFront.setPower(1);
-        r.leftBack.setPower(1);
-        r.leftFront.setPower(1);
-        try {
-            Thread.currentThread().sleep(time);
-        } catch (InterruptedException e) {}
-        stopMotors();
-    }
-    public void backward(int time) {
-        r.rightBack.setPower(1);
-        r.rightFront.setPower(-1);
-        r.leftBack.setPower(-1);
-        r.leftFront.setPower(-1);
-        try {
-            Thread.currentThread().sleep(time);
-        } catch (InterruptedException e) {}
-        stopMotors();
-    }
-    public void turnRight(int time) {
-        r.rightBack.setPower(-1);
-        r.rightFront.setPower(1);
-        r.leftBack.setPower(-1);
-        r.leftFront.setPower(-1);
-        try {
-            Thread.currentThread().sleep(time);
-        } catch (InterruptedException e) {}
-        stopMotors();
-    }
-    public void turnLeft(int time) {
-        r.rightBack.setPower(1);
-        r.rightFront.setPower(-1);
-        r.leftBack.setPower(1);
-        r.leftFront.setPower(1);
-        try {
-            Thread.currentThread().sleep(time);
-        } catch (InterruptedException e) {}
-        stopMotors();
-    }
-    public void mecanumStrafeLeft(int time) {
-        r.leftFront.setPower(-1);
-        r.leftBack.setPower(1);
-        r.rightFront.setPower(1);
-        r.rightBack.setPower(1);
-        try {
-            Thread.currentThread().sleep(time);
-        } catch (InterruptedException e) {}
-        stopMotors();
-    }
-    public void mecanumStrafeRight(int time) {
-        r.leftFront.setPower(1);
-        r.leftBack.setPower(-1);
-        r.rightFront.setPower(-1);
-        r.rightBack.setPower(-1);
-        try {
-            Thread.currentThread().sleep(time);
-        } catch (InterruptedException e) {}
-        stopMotors();
-    }
+    // turn right
+    r.MecanumDriveRotateLeft(ppower, 3000);
+    // drive forward to align with wall
+    r.MecanumDriveForward(ppower, 5000);
+    // strafe to depot
+    r.MecanumDriveStrafeLeft(ppower, 10000);
 }
+*/
--- ftc_app-master M-Set Bettafish 2018 - 2019/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOp_0.java
+++ ftc_app-master M-Set Bettafish 2018 - 2019/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOp_0.java
@@ -122,4 +122,58 @@ public class TeleOp_0 extends LinearOpMode {
 		r.leftFront.setPower(0);
 	}
 
+
+
+	// OBSOLETE FROM HERE BELOW
+	// pre-coded drive Speeds
+	// Mecanum power is determined by Speed, Turn, and Strafe
+	public void MecanumDrive() {
+		double Speed = -gamepad1.left_stick_y;
+		double Turn = gamepad1.left_stick_x;
+		double Strafe = gamepad1.right_stick_x;
+		double MAX_SPEED = 1.0;
+		holonomic(Speed, Turn, Strafe, MAX_SPEED);
+	}
+// Front, Back, rightstrafe, leftstrafe
+	public void holonomic(double Speed, double Turn, double Strafe, double MAX_SPEED) {
+//EQ:      Left Front = +Speed + Turn - Strafe      Right Front = +Speed - Turn + Strafe
+//EQ:      Left Rear  = +Speed + Turn + Strafe      Right Rear  = +Speed - Turn - Strafe
+
+		double Magnitude = abs(Speed) + abs(Turn) + abs(Strafe);
+		Magnitude = (Magnitude > 1) ? Magnitude : 1; //Set scaling to keep -1,+1 range
+
+		r.leftFront.setPower(scaleInput(Speed) + scaleInput(Turn) - scaleInput(Strafe));
+		if (r.leftBack != null) {
+			r.leftBack.setPower(scaleInput(Speed) + scaleInput(Turn) + scaleInput(Strafe));
+		}
+		r.rightFront.setPower(scaleInput(Speed) - scaleInput(Turn) + scaleInput(Strafe));
+		if (r.rightBack != null) {
+			r.rightBack.setPower(scaleInput(Speed) - scaleInput(Turn) - scaleInput(Strafe));
+		}
+	}
+
+	double scaleInput(double dVal)  {
+		double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
+				0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
+		// get the corresponding index for the scaleInput array.
+		int index = (int) (dVal * 16.0);
+		// index should be positive.
+		if (index < 0) {
+			index = -index;
+		}
+		// index cannot exceed size of array minus 1.
+		if (index > scaleArray.length) {
+			index = scaleArray.length;
+		}
+		// get value from the array.
+		double dScale = 0.0;
+		if (dVal < 0) {
+			dScale = -scaleArray[index];
+		} else {
+			dScale = scaleArray[index];
+		}
+		// return scaled value.
+		return dScale;
+	}
+
 }

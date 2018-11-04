d739213759626e685f6c0d3fdb6804de2b4da72f
Jack Liu <liujack@google.com>
2018-11-04 10:12:21 -0800

    test

--- ftc_app-master M-Set Bettafish 2018 - 2019/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoOpSample.java
+++ ftc_app-master M-Set Bettafish 2018 - 2019/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoOpSample.java
@@ -4,6 +4,6 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 public class AutoOpSample extends LinearOpMode {
     public void runOpMode() {
-        // for later
+        // for later change
     }
 }
